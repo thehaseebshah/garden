c514e594bf8d0811ae2e637ce8abb152b1bba099
Haseeb Shah <thehaseebshah@proton.me>
2026-01-05 19:28:45 +0500

    Copy fonts in build

--- .eleventy.js
+++ .eleventy.js
@@ -527,6 +527,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/site/img");
+  eleventyConfig.addPassthroughCopy("src/site/fonts");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
